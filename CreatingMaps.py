648754f29346854887e3778ed261c4ae9851ad8a
Trent Chipman <trentchip@gmail.com>
2024-12-24 23:25:37 -0700

    Added gap in wall funciton Making map generator add random gaps in the boxes

--- CreatingMaps.py
+++ CreatingMaps.py
@@ -1,6 +1,7 @@
 #Creating Maps
 # Outer walls
 import math
+import random
 WIDTH, HEIGHT = 800, 600
 
 ver_dec_amount = 60
@@ -46,7 +47,7 @@ levelwall = [
 ]
 print(levelwall)
 print("CustomWalls")
-WIDTH, HEIGHT = 800, 600
+WIDTH, HEIGHT = 790, 590
 
 hor_walls = 17
 ver_walls = 14
@@ -70,20 +71,89 @@ customwalls = [
     (0, 0, 10, 600),           # Left wall
     (790, 0, 10, 600),         # Right wall
 ]
+
+
+def add_gap_to_hor_wall(wall, gap_start, gap_width):
+    x, y, width, height = wall
+    left_wall = (x, y, gap_start, height)
+    right_wall = (gap_start + gap_width + x, y, width - (gap_start + gap_width), height)
+    return [left_wall, right_wall]
+    
+def add_gap_to_ver_wall(wall, gap_start, gap_width):
+    x, y, width, height = wall
+    top_wall = (x, y, width, gap_start + y)
+    bottom_wall = (x, gap_start + gap_width, width, height - (gap_start - y + gap_width))
+    return [top_wall, bottom_wall]
+    
+def add_gap_to_wall(wall, gap_start, gap_width):
+    x, y, width, height = wall
+    if width > height:
+        left_wall = (x, y, gap_start - x, height)
+        right_wall = (gap_start + gap_width, y, width - (gap_start - x + gap_width), height)
+        return [left_wall, right_wall]
+    elif height > width:
+        left_wall = (x, y, width, gap_start - y)
+        right_wall = (x, gap_start + gap_width, width, height - (gap_start - y + gap_width))
+        return [left_wall, right_wall]
+
+
 #Create horizontal walls
 for i in list(range(math.ceil(hor_walls/2))):
+    #Calculate positions for all walls
     new_wall = (hor_start + hor_dec_amount*i, vert_start + ver_dec_amount*i, hori_length - hor_dec_amount*(i*2), Width)
+    if not any(val < 0 for val in new_wall):#check they don't have any negative numbers
+        #Then also split wall into 3 sub walls for 4 biggest rings to have gaps
+        if i == 0:
+            length_in = new_wall[2]//3 -15
+            new_walls = add_gap_to_hor_wall(new_wall, length_in, 30)
+            new_wall = new_walls[0]
+            new_walls1 = add_gap_to_hor_wall(new_walls[1], length_in, 30)
+            customwalls.append(new_wall)
+            for wall in new_walls1:
+                customwalls.append(wall)
+        elif i < 4:#random gaps for these rings
+            new_walls = add_gap_to_hor_wall(new_wall, random.randint(10, new_wall[2]//3), random.randint(30, 40))
+            new_wall = new_walls[0]
+            new_walls1 = add_gap_to_hor_wall(new_walls[1],random.randint(0, new_walls[1][2]-10), random.randint(30, 40))
+            customwalls.append(new_wall)
+            for wall in new_walls1:
+                customwalls.append(wall)
+        elif i < hor_walls//2:#and one gap for any smaller rings
+            new_walls = add_gap_to_hor_wall(new_wall, random.randint(0, new_wall[2]), random.randint(30, 40))
+            for wall in new_walls:
+                customwalls.append(wall)
+        else:#Keep wall as is for the last one
+            customwalls.append(new_wall)
+    new_wall = (hor_start + hor_dec_amount*i, (HEIGHT -vert_start - ver_dec_amount*i)-10, hori_length - hor_dec_amount*(i*2), Width)
     if not any(val < 0 for val in new_wall):
-        customwalls.append(new_wall)
-    new_wall = (hor_start + hor_dec_amount*i, (HEIGHT -vert_start - ver_dec_amount*i), hori_length - hor_dec_amount*(i*2), Width)
-    if not any(val < 0 for val in new_wall):
-        customwalls.append(new_wall)
+        if i == 0:
+            length_in = new_wall[2]//3 -15
+            new_walls = add_gap_to_hor_wall(new_wall, length_in, 30)
+            new_wall = new_walls[0]
+            new_walls1 = add_gap_to_hor_wall(new_walls[1], length_in, 30)
+            customwalls.append(new_wall)
+            for wall in new_walls1:
+                customwalls.append(wall)
+        elif i < 4:#random gaps for these rings
+            new_walls = add_gap_to_hor_wall(new_wall, random.randint(10, new_wall[2]//3), random.randint(30, 40))
+            new_wall = new_walls[0]
+            new_walls1 = add_gap_to_hor_wall(new_walls[1],random.randint(0, new_walls[1][2]-10), random.randint(30, 40))
+            customwalls.append(new_wall)
+            for wall in new_walls1:
+                customwalls.append(wall)
+        elif i < hor_walls//2:#and one gap for any smaller rings
+            new_walls = add_gap_to_hor_wall(new_wall, random.randint(0, new_wall[2]), random.randint(30, 40))
+            for wall in new_walls:
+                customwalls.append(wall)
+        else:#Keep wall as is for the last one
+            customwalls.append(new_wall)
 #Create vertical walls
 for i in list(range(math.ceil(ver_walls/2))):
     new_wall = (hor_start + hor_dec_amount*i, vert_start + ver_dec_amount*i, Width, vert_length - ver_dec_amount*(i*2))
     if not any(val < 0 for val in new_wall):
+
         customwalls.append(new_wall)
-        new_wall = ((WIDTH -hor_start - hor_dec_amount*i), vert_start + ver_dec_amount*i, Width, 10 +vert_length - ver_dec_amount*(i*2))
+    new_wall = ((WIDTH -hor_start - hor_dec_amount*i - 10), vert_start + ver_dec_amount*i, Width, 10 +vert_length - ver_dec_amount*(i*2))
     if not any(val < 0 for val in new_wall):
         customwalls.append(new_wall)
 
